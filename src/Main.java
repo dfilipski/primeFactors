5028f46aed950e12b26f3e45e9a076f16b8932c8
dfilipski <danny.filipski@gmail.com>
2021-07-05 20:01:59 -0400

    Implement sieve of eratosthenes

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,44 @@ public class Main {
         System.out.println(Arrays.toString(factors));
     }
 
+    public static int[] getPrimes(int max) {
+        boolean[] A = new boolean[max + 1];
+        Arrays.fill(A, true);
+        A[0] = false;
+        A[1] = false;
+
+        for (int p = 2; p * p <= max; ++p) {
+            if (A[p] == true) {
+                for (int i = p * p; i <= max; i += p) {
+                    A[i] = false;
+                }
+            }
+        }
+
+        int[] primes = new int[countTrue(A)];
+        int j = 0;
+        for (int i = 0; i < A.length; ++i) {
+            if (A[i] == true) {
+                primes[j] = i;
+                ++j;
+            }
+        }
+
+        return primes;
+    }
+
+    public static int countTrue(boolean[] A) {
+        int count = 0;
+        for (boolean b : A) {
+            if (b == true)
+                ++count;
+        }
+        return count;
+    }
+
     public static int[] primeFactor(int num) {
-        int[] factors = new int[1024];
+        int[] factors = new int[num];
+        int[] primes = getPrimes(num);
 
         return factors;
     }
